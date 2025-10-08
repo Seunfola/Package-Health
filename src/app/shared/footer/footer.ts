c86cd5b9eaea20b3aa0b77f9d827adb9e66dd234
seunfola <seunfola1@gmail.com>
2025-10-08 16:03:27 +0100

    fixed the homepage layout and all it details

--- src/app/shared/footer/footer.ts
+++ src/app/shared/footer/footer.ts
@@ -3,7 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer',
   imports: [],
+  standalone: true,
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
-export class Footer {}
+export class Footer {
+  currentYear: number = new Date().getFullYear();
+
+  footerLinks = [
+    { title: 'Company', links: ['About Us', 'Careers', 'Blog'] },
+    { title: 'Product', links: ['Features', 'Pricing', 'API'] },
+    { title: 'Resources', links: ['Documentation', 'Support', 'Legal'] },
+  ];
+}
