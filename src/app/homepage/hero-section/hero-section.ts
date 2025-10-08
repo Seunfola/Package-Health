8b9b1047a13734ef6c5289d124788ae6566e45ee
seunfola <seunfola1@gmail.com>
2025-10-08 17:18:09 +0100

    developing the homepage
    Developing the hero section of the homepage
    Fixing the footer layout in the Layout to allow proper responsiveness
    Adjusting the other layout details

---
+++ src/app/homepage/hero-section/hero-section.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-section',
+  imports: [],
+  standalone: true,
+  templateUrl: './hero-section.html',
+  styleUrl: './hero-section.css',
+})
+export class HeroSection {}
---
+++ src/app/homepage/updates/updates.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-updates',
+  imports: [],
+  templateUrl: './updates.html',
+  styleUrl: './updates.css'
+})
+export class Updates {
+
+}
